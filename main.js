ee9ae51beab3038535ba0ce8ecdfbd24c96823f0
Felipe099 <lipepagodart99@gmail.com>
2023-01-04 18:02:21 -0300

    update background image in the pages

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
+const linkHome = document.querySelector('.linkHome');
+const linkAbout = document.querySelector('.linkAbout');
+const linkExplorer = document.querySelector('.linkExplorer');
+const bodyHome = document.querySelector('body');
+
+linkHome.addEventListener('click', () => {
+    linkHome.classList.add('active');
+    linkAbout.classList.remove('active');
+    linkExplorer.classList.remove('active');
+    bodyHome.classList.add('bodyHome');
+    bodyHome.classList.remove('bodyAbout');
+    bodyHome.classList.remove('bodyExplorer');
+});
+
+linkAbout.addEventListener('click', () => {
+    linkAbout.classList.add('active');
+    linkHome.classList.remove('active');
+    linkExplorer.classList.remove('active');
+    bodyHome.classList.add('bodyAbout');
+    bodyHome.classList.remove('bodyHome');
+    bodyHome.classList.remove('bodyExplorer');
+});
+
+linkExplorer.addEventListener('click', () => {
+    linkExplorer.classList.add('active');
+    linkHome.classList.remove('active');
+    linkAbout.classList.remove('active');
+    bodyHome.classList.add('bodyExplorer');
+    bodyHome.classList.remove('bodyAbout');
+    bodyHome.classList.remove('bodyHome');
+});
+
 const routes = {
     '/': '/pages/home.html',
     '/about': '/pages/about.html',
@@ -8,7 +40,7 @@ function route(event) {
     event = event || window.event;
     event.preventDefault();
 
-    window.history.pushState({}, '', event.target.href)
+    window.history.pushState({}, '', event.target.href);
 
     hundle();
 }
@@ -20,8 +52,8 @@ function hundle() {
     fetch(route)
         .then((data) => data.text())
         .then((html) => (document.querySelector('#app').innerHTML = html));
-        
 }
 hundle();
 
 window.onpopstate = () => hundle();
+
